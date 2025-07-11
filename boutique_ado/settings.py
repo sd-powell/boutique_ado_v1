81eb5a42daca879812ab1e09731ce9fe4787fd8f
sd-powell <kf210rhd@students.codeinstitute.net>
2025-07-11 21:15:21 +0100

    fix indentation in settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -207,7 +207,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVEOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-        DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+    DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
