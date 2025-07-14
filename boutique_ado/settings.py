f0c42579620fb803e6d995c212e0189cd1b7ddeb
sd-powell <kf210rhd@students.codeinstitute.net>
2025-07-14 09:43:18 +0100

    refactor login and registration on mobile devices

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEVELOPMENT', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',') + ['127.0.0.1']
 
 
 INSTALLED_APPS = [
